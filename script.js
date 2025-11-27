4c0064049958f9c64ded2a76216022ae66865539
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-11-27 10:14:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ document.querySelector(".github-btn").addEventListener("click", () => {
   chrome.tabs.create({ url: "https://github.com/loading617/DownloadYouTubeComment" });
 });
 
-const bugReport = document.querySelector(".btn");
+const bugReportBtn = document.querySelector(".btn");
 bugReportBtn.innerHTML = chrome.i18n.getMessage("bugReport");
-bugReport.addEventListener("click", () => {
+bugReportBtn.addEventListener("click", () => {
   chrome.tabs.create({ url: "https://github.com/loading617/DownloadYouTubeComment/issues" });
 });
 
-const changelog = document.querySelector(".btn");
+const changelogBtn = document.querySelector(".btn");
 changelogBtn.innerHTML = chrome.i18n.getMessage("changelog");
-changelog.addEventListener("click", () => {
+changelogBtn.addEventListener("click", () => {
   chrome.tabs.create({ url: "https://downloadyoutubecomment.com/changelog" });
 });
